8c130ce821f3e7a19a0eb8694278c75b461370dc
Ashleigh McMahon <ashleighmc@me.com>
2023-09-09 11:22:37 -0600

    Lab Complete

--- index.js
+++ index.js
@@ -1,2 +1,29 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = (num1*num2)
+console.log(multiply);
+
+
+let random = 8;  
+Math.round(Math.random()*10)+1;
+
+let num3 = 9;
+let num4 = 5;
+let mod = (num3%num4);
+console.log (mod)
+
+let max= 20
+Math.max (5,10,15,20)
+console.log (max)
+
+
+
+
+
+
+
+
+
+
